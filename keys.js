17937c46cd8af2b3d10c52d13e1ed7f64d2d7bdd
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-16 10:30:14 +0530

    creating the config file

--- keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-    google:{
-        clientID: "186157319007-nutivvfrah79lj2jcdkpmliiennsqdhf.apps.googleusercontent.com",
-        clientSecret:"W5hKnMwPM27vKSslO_-DSqHw"
-    }
-}
