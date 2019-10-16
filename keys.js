de9548c127b42e5bd6464ea15080fd6b248bcfed
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-16 10:24:17 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    google:{
+        clientID: "186157319007-nutivvfrah79lj2jcdkpmliiennsqdhf.apps.googleusercontent.com",
+        clientSecret:"W5hKnMwPM27vKSslO_-DSqHw"
+    }
+}
