9a34f37970f651fe59b8e400781235df26095b8e
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-10-16 10:23:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://varun:Mustang@cluster0-bhfp5.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri,{ useUnifiedTopology: true }, { useNewUrlParser: true });
+const app = express();
+const port = 8100;
+client.connect(err => {
+    const col = client.db("careers").collection('jobs');
+    console.log("connect");
+    if (err)
+    return console.log(err);
+
+    //routes
+    app.use(cors());
+    require('./routes')(app, col);
+    app.listen(port, () => {
+        console.log("Port 8100 running on browser...");
+    });
+});
---
+++ routes.js
@@ -0,0 +1,34 @@
+const passportsetup = require('./config/passport');
+const passport = require("passport")
+module.exports = (app, col) => {
+    app.get("/api", (req, res) => {
+        col.findOne({}, function(err, document) {
+            console.log(document);
+            let loca=JSON.stringify(document)
+             loca=JSON.parse(loca)
+            res.send(document);
+            console.log(loca)
+            
+          });
+    });
+
+    app.get("/login",(req,res) => {
+        res.send("login");
+    })
+
+    app.get("/logout",(req,res) => {
+        res.send("logout");
+    })
+        
+    app.get("/google",passport.authenticate('google',{
+        scope:['profile']
+    }))
+    
+ }
+
+ 
+ 
+ 
+
+ 
+
